e799e83cefb37cbb38e464af9acb75e970e6ea97
Trang Huynh <huynhphuongtrang309@gmail.com>
2024-01-29 16:55:56 +0700

    [First Microservice] Create Auction DTOs

---
+++ src/AuctionService/RequestHelpers/MappingProfile.cs
@@ -0,0 +1,18 @@
+
+using AuctionSerice.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionSerice.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
