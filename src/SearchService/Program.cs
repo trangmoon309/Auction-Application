b0e6a5f1c439841d70c083bc7d93778a4a364aa3
Trang Huynh <huynhphuongtrang309@gmail.com>
2024-02-15 17:36:59 +0700

    [Second Microservice] - Search Service with MongoDB

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using MongoDB.Driver;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () => 
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+app.Run();
+
+// If Auction Service down, thì chúng ta sẽ handle transient http error exception
+// Ta sẽ keep retry after 3s
+// Nó sẽ retry cho tới khi mình start Auction Service
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
