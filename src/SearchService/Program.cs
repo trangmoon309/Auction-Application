2eef00347095feb6568cb54ccb23cb01957890fa
Trang Huynh <huynhphuongtrang309@gmail.com>
2024-02-15 22:27:37 +0700

    [Rabbit MQ] Configuration

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using Polly;
 using Polly.Extensions.Http;
@@ -10,6 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x => 
+{
+    x.UsingRabbitMq((context, cfg) => 
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
